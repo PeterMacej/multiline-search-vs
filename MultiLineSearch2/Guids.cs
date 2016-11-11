5f7042100320cbfc4371d74192487ee0f8ec4436
Peter Macej <macej.peter@gmail.com>
2016-11-11 10:08:31 +0000

    Converted to VS 2015 and added the first draft of C# package version.

---
+++ MultiLineSearch2/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.MultiLineSearch2
+{
+    static class GuidList
+    {
+        public const string guidMultiLineSearch2PkgString = "b996608b-c716-452d-a711-7cfad5c874c5";
+        public const string guidMultiLineSearch2CmdSetString = "f9d2a289-d7f7-44df-a331-f39b8e29d869";
+        public const string guidToolWindowPersistanceString = "0c0f322c-3ac7-4632-8af2-e707597f9fb1";
+
+        public static readonly Guid guidMultiLineSearch2CmdSet = new Guid(guidMultiLineSearch2CmdSetString);
+    };
+}
