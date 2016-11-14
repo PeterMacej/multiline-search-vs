5cc05659e8b2aa89cf5e7dbfdbcb1c27b61595f4
Peter Macej <macej.peter@gmail.com>
2016-11-14 15:44:20 +0000

    included some helper files

---
+++ MultiLineSearch2/ResetVsExperimental2013.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VSSDK\VisualStudioIntegration\Tools\Bin\CreateExpInstance.exe" /Reset /VSInstance=12.0 /RootSuffix=Exp
+PAUSE
---
+++ MultiLineSearch2/ResetVsExperimental2015.bat
@@ -0,0 +1,4 @@
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\14.0Exp_Config" /f  
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VSSDK\VisualStudioIntegration\Tools\Bin\CreateExpInstance.exe" /Reset /VSInstance=14.0 /RootSuffix=Exp
+
+pause
